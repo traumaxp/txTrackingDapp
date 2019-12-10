718da707ce3945dec34038ec2f011e77897b166c
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2019-12-10 11:50:09 +0100

    First commit

---
+++ frontend/src/components/Chat/Message.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        sent
+        v-if="!auth"
+      ><span>{{ message.user }}</span>
+      <p>{{ DateMessage }}</p>
+      <p>{{ message.text }}</p></q-chat-message>
+      <q-chat-message
+      :text="['Received message']"
+      />
+      <q-chat-message
+        v-if="!auth"
+      ><span>{{ message.user }}</span>
+      <p>{{ DateMessage }}</p>
+      <p>{{ message.text }}</p></q-chat-message>
+      <q-chat-message
+      :text="['Received message']"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message', 'index'],
+  computed: {
+    DateMessage () {
+      return this.message.createdAt
+    }
+  }
+}
+</script>
