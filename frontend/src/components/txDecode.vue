8b8dc02d2b53ce91b8c8639fcd28a37538541ad4
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-03-06 13:05:06 +0100

    Update decodetx()

--- frontend/src/components/txDecode.vue
+++ frontend/src/components/txDecode.vue
@@ -12,6 +12,9 @@
       <q-card-section>
         {{renderData}}
       </q-card-section>
+      <q-card-section>
+        {{renderData}}
+      </q-card-section>
     </q-card>
   </div>
 </template>
@@ -22,17 +25,23 @@ export default {
   data: () => ({
     valid: false,
     txHash: '',
-    renderData: ''
+    renderData: {
+      logs: ''
+    }
   }),
   computed: {
   },
   methods: {
+    getPastEvent () {
+    },
     decodeTx () {
       var web3 = new Web3(window.web3.currentProvider)
-      web3.eth.getTransaction(this.txHash)
+      web3.eth.getTransactionReceipt(this.txHash)
         .then(result => {
           console.log(result)
-          this.renderData = result
+          this.renderData.logs = result.logs[0]
+          console.log(result.logs[0].address)
+          web3.eth.getTransactionReceipt(result.logs[0].address).then(console.log)
         })
     }
   }
