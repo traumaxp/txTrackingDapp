70719d3ef1c2a05eb31f6f19b405efc97eac5114
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-03-15 20:01:38 +0100

    Update getTokensBalance

--- frontend/src/util/getTokensBalances.js
+++ frontend/src/util/getTokensBalances.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 
 let tokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f'
-let walletAddress = '0xB74fc3B69f626226f7F1c53D9D6D340AC291d481'
+// let walletAddress = '0xB74fc3B69f626226f7F1c53D9D6D340AC291d481'
 
 let minABI = [
   {
@@ -28,15 +28,22 @@ let minABI = [
 let getTokensBalance = new Promise(function (resolve, reject) {
   // Check for injected web3 (mist/metamask)
   var web3 = new Web3(window.web3.currentProvider)
-  let contract = new web3.eth.Contract(minABI, tokenAddress)
-  // Start
-  contract.methods.balanceOf(walletAddress).call().then(daiBalance => {
-    contract.methods.decimals().call().then((decimals) => {
-      daiBalance = daiBalance / (10 ** decimals)
-      const result = Object.assign({}, daiBalance, { daiBalance })
-      resolve(result)
-    })
-  })
+  resolve(
+    web3.eth.getAccounts()
+      .then(result => {
+        return new Promise(function (resolve, reject) {
+          let contract = new web3.eth.Contract(minABI, tokenAddress)
+          // Start
+          contract.methods.balanceOf(result[0]).call().then(daiBalance => {
+            contract.methods.decimals().call().then((decimals) => {
+              daiBalance = daiBalance / (10 ** decimals)
+              const result = Object.assign({}, daiBalance, { daiBalance })
+              resolve(result)
+            })
+          })
+        })
+      })
+  )
 })
 
 export default getTokensBalance
